81e3cd35b89b4887497e99359779d8844322271b
ShahierNashaat <shahiernashaat97@gmail.com>
2022-03-08 16:16:25 +0200

    Create Food and Recipe Models

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,43 @@
+class FoodsController < ApplicationController
+  before_action :set_food, only: %i[show edit update destroy]
+
+  # GET /foods or /foods.json
+  def index
+    @foods = Food.where(user_id: params[:user_id])
+  end
+
+  # GET /foods/new
+  def new
+    @food = Food.new
+  end
+
+  # POST /foods or /foods.json
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    if @food.save
+      redirect_to "/users/#{current_user.id}/foods/"
+    else
+      render :new, alert: 'Sorry something went wrong'
+    end
+  end
+
+  # DELETE /foods/1 or /foods/1.json
+  def destroy
+    food = Food.find(params[:id])
+    food.destroy
+    redirect_to "/users/#{current_user.id}/foods/"
+  end
+
+  private
+
+  def set_food
+    Food.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  resources :recipes
+  default_url_options :only_path => true
   devise_for :users
+
+  resources :users do
+    resources :foods, only: %i[index new create destroy]
+    resources :recipes
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
+  root "foods#index"
 end
