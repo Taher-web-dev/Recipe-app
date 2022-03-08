abba69ce234ea50c4fbab718a5e412e6dc692d90
Taher <ttaherhagui@gmail.com>
2022-03-08 14:35:03 +0100

    generate recipe_food model and its association with references tables.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recipes
   has_many :foods
+  has_many :recipe_foods
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
