d90a2ae16e5eabd859e3072c40563fc2cfb72eb0
Taher <ttaherhagui@gmail.com>
2022-03-08 14:21:30 +0100

    generate food model and set his association with referenced tables.

---
+++ app/models/food.rb
@@ -0,0 +1,4 @@
+class Food < ApplicationRecord
+  belongs_to :user
+  validates :price, numericality: { greater_than: 0 }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recipes
+  has_many :foods
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   
