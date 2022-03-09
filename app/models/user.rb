2aaa9af16c0666d40dafd06fb0f4a7f8397d1cf4
Taher <ttaherhagui@gmail.com>
2022-03-09 14:35:25 +0100

    resolve conflicts with public recipes branch.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :recipes
-  has_many :foods
-  has_many :recipe_foods
+  has_many :recipes, dependent: :destroy
+  has_many :foods, dependent: :destroy
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
