6087dcef7e8c2d3a7cf08c5c7f1dde30577e7c3f
Taher <ttaherhagui@gmail.com>
2022-03-10 11:04:36 +0100

    valid

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'], port:3000}
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST']}
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
     password:       ENV['SENDMAIL_PASSWORD'],
