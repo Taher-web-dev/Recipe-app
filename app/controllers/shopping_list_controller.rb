998ef5d563b11b9fdcc07ca0bade425356b8ef11
Taher <ttaherhagui@gmail.com>
2022-03-10 16:41:52 +0100

    fix linter.

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -10,16 +10,16 @@ class ShoppingListController < ApplicationController
       rfoods.each do |f|
         rq = f.quantity
         eq = InventoryFood.find_by(food_id: f.food_id, inventory_id: inv_d).quantity
-        dq = rq - eq 
-        if dq > 0
-          @missing_foods.push(Food.find(f.food_id))
-          @quantity.push(dq)
-          @prices.push(dq * Food.find(f.food_id).price)
-        end
+        dq = rq - eq
+        next unless dq.positive?
+
+        @missing_foods.push(Food.find(f.food_id))
+        @quantity.push(dq)
+        @prices.push(dq * Food.find(f.food_id).price)
       end
     end
     @amount_item = @missing_foods.size
-    @total_value = 0 
+    @total_value = 0
     @prices.each do |p|
       @total_value += p
     end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,5 +1,5 @@
 class Inventory < ApplicationRecord
   belongs_to :user
-  validates :name, presence:true
+  validates :name, presence: true
   has_many :inventory_food
 end
