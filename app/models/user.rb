fc128e51d793d998b1decf08b4660dc4dfe68b44
Taher <ttaherhagui@gmail.com>
2022-03-12 15:27:37 +0100

    update user association.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recipes, dependent: :destroy
   has_many :foods, dependent: :destroy
-  has_one :inventories, dependent: :destroy
+  has_one :inventorys, dependent: :destroy
   validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
